9b7f6536b1c8ff6c7758204583312e74f7ef073b
Ibrahim Olawoyin <woyin365@gmail.com>
2025-09-27 21:55:50 +0000

    a script that performs specified operations with dates and times.

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,6 +6,6 @@ def display_current_datetime():
 
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
-    today = datetime.datetime.now()
-    future_date = today + datetime.timedelta(days=number_of_days)
+    current_date = datetime.datetime.now()
+    future_date = current_date + datetime.timedelta(days=number_of_days)
     print(future_date.strftime("Future date: %Y-%m-%d"))
