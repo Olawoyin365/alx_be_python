4dd98fca674caaaa6b6b83ead4b4319eb992c841
Ibrahim Olawoyin <woyin365@gmail.com>
2025-10-10 08:37:06 +0000

    python script with a base class Book and two derived classes, EBook and PrintBook, showcasing inheritance and a Library class tto demonstrate Composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,6 +3,9 @@ class Book:
         self.title = str(title)
         self.author = str(author)
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
